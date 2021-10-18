805675b7ee5c79c1a0c43fea4b6827b398eec3c1
dr0er <ernestxxx123@gmail.com>
2021-10-18 20:31:15 +0200

    Add final routes

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -20,7 +20,7 @@ const HeroSection = () => {
     setHover(!hover);
   };
   return (
-    <HeroContainer>
+    <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
@@ -36,6 +36,11 @@ const HeroSection = () => {
             onMouseLeave={onHover}
             primary="true"
             dark="true"
+            smooth={true}
+            duration="500"
+            spy={true}
+            exact="true"
+            offset={-80}
           >
             Get started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
