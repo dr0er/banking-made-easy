8bb2929ebc37e9ce4600a32606abc7369c59e984
dr0er <ernestxxx123@gmail.com>
2021-10-18 20:22:51 +0200

    Add location feature

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,13 @@
 import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
 import SignIn from "../components/Signin";
 
 const SigninPage = () => {
   return (
-    <div>
+    <>
+      <ScrollToTop />
       <SignIn />
-    </div>
+    </>
   );
 };
 
