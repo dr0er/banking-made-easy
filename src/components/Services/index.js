f27b69c3174f6886bcd8ddde515f6a4be866056a
dr0er <ernestxxx123@gmail.com>
2021-10-18 13:38:09 +0200

    Fix header and display section

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -14,32 +14,31 @@ import {
 
 const Services = () => {
   return (
-    <ServicesContainer is="services">
-      <ServicesH1>
-        <ServicesWrapper>
-          <ServicesCard>
-            <ServicesIcon src={Icon1} />
-            <ServicesH2>Reduce expense</ServicesH2>
-            <ServicesP>
-              We help reduce your fees and increase your overall revenue.
-            </ServicesP>
-          </ServicesCard>
-          <ServicesCard>
-            <ServicesIcon src={Icon2} />
-            <ServicesH2>Virtual Offices</ServicesH2>
-            <ServicesP>
-              You can access our platform online anywhere in the world.
-            </ServicesP>
-          </ServicesCard>
-          <ServicesCard>
-            <ServicesIcon src={Icon3} />
-            <ServicesH2>Premium Benefits</ServicesH2>
-            <ServicesP>
-              Unlock our special membership card that returns 5% cash back.
-            </ServicesP>
-          </ServicesCard>
-        </ServicesWrapper>
-      </ServicesH1>
+    <ServicesContainer id="services">
+      <ServicesH1>Our services</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1} />
+          <ServicesH2>Reduce expense</ServicesH2>
+          <ServicesP>
+            We help reduce your fees and increase your overall revenue.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon2} />
+          <ServicesH2>Virtual Offices</ServicesH2>
+          <ServicesP>
+            You can access our platform online anywhere in the world.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon3} />
+          <ServicesH2>Premium Benefits</ServicesH2>
+          <ServicesP>
+            Unlock our special membership card that returns 5% cash back.
+          </ServicesP>
+        </ServicesCard>
+      </ServicesWrapper>
     </ServicesContainer>
   );
 };
